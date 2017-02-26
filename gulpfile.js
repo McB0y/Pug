5e41fdb34d24bfb50bc430494ed90a151cb8992d
McB0y <rafael.cisneros.gomez@gmail.com>
2017-02-25 20:29:14 -0600

    Pug Cambios
    Cambios en los templates y estructura del archivo.

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,15 @@ const pug = require('gulp-pug');
 //creando nuestras tareas.
 // NOTE: La función pug debe recibir un objeto con las opcciones correspondientes a l salida deseada del file
 gulp.task('pug', ()=>{
-   gulp.src('./dev/views/*.pug')
+   gulp.src('./dev/views/index.pug')
        .pipe(pug({
           pretty : true //esta opción nos regresa una version sin minificar
        }))
        .pipe(gulp.dest('./dist/'))
 });
+
+// NOTE: Tarea por default
+
+gulp.task('default', ()=>{
+   gulp.watch('./dev/views/**/*.pug', ['pug']);
+})
